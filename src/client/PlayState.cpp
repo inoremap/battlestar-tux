d9ae9ff1fbf6af9a064046f04acd485e63ae2dda
mantislovestux <mantislovestux@16c49784-e034-0410-a7bd-bf3b583398bf>
2010-04-28 02:24:25 +0000

    Bug fix: Weapon timeout was not specified in milliseconds.
    git-svn-id: https://battlestar-tux.googlecode.com/svn/trunk@278 16c49784-e034-0410-a7bd-bf3b583398bf

--- src/client/PlayState.cpp
+++ src/client/PlayState.cpp
@@ -68,7 +68,7 @@ void PlayState::enter() {
     mPlayer->addCoreHexCell(coreCell, 0, 0);
     EnergyCell* energyCell = new EnergyCell("PlayerShip:EnergyCell", 1, 500, 100, 500);
     mPlayer->addHexCell(energyCell, 0, 1);
-    WeaponCell* weaponCell = new WeaponCell("PlayerShip:WeaponCell", 1, 300, 50, 50, 0.1);
+    WeaponCell* weaponCell = new WeaponCell("PlayerShip:WeaponCell", 1, 300, 50, 50, 0.1 * 1000);
     mPlayer->addHexCell(weaponCell, 0, -1);
 
     // Add AI ship
